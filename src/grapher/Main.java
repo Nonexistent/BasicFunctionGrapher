f4a42b4c01a3d3bfa31e0ee75c6bf485e58589f5
Nonexistent <chunkiangvinegar@gmail.com>
2014-01-28 13:39:56 -0700

    test 2

--- src/grapher/Main.java
+++ src/grapher/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	/*
 	 * Author: Nonexistent
 	 * Started on: January 16th 2014
-	 * Last Update (Requires logging in): http://www.powerbot.org/community/topic/1142029-a-basic-function-grapher/
+	 * Last Update (Loggin in required): http://www.powerbot.org/community/topic/1142029-a-basic-function-grapher/
 	 * 
 	 */
 	
